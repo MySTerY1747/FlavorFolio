8c15eaa89e1a9314ff60053b42f51044875523f3
Stefanos Mytilinaios <stefanosmyt@tutamail.com>
2025-03-07 14:22:44 +0000

    Added static images directory

--- flavorfolio/flavorfolio/settings.py
+++ flavorfolio/flavorfolio/settings.py
@@ -16,7 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
-print(TEMPLATE_DIR)
+STATIC_DIR = os.path.join(BASE_DIR, "static")
 
 
 # Quick-start development settings - unsuitable for production
@@ -127,3 +127,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
