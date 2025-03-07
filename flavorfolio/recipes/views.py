4063626abc6715f83404c86cb452a39bde2d1ce3
Stefanos Mytilinaios <stefanosmyt@tutamail.com>
2025-03-07 13:55:17 +0000

    Added basic view and url functionality

--- flavorfolio/recipes/views.py
+++ flavorfolio/recipes/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+def index(request):
+    context_dict = {"body_block": "This is the index page.", "title_block": "Index"}
+    response = render(request, "recipes/base.html", context=context_dict)
+    return response
+
+
+def about(request):
+    context_dict = {"body_block": "This is the about page.", "title_block": "About"}
+    response = render(request, "recipes/base.html", context=context_dict)
+    return response
