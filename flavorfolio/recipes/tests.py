c45bb6aa60d096b41ca6c8f213be1186c70f2e7b
Ross Munro <2564202m@student.gla.ac.uk>
2025-03-24 23:41:14 +0000

    add simple test

--- flavorfolio/recipes/tests.py
+++ flavorfolio/recipes/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from recipes.models import UserProfile, Recipe
+from datetime import datetime
 
 # Create your tests here.
+    
+def add_recipe(title, ingredients = "", instructions = ""):
+    user = User.objects.create_user(username="testuser", password="wz5xe6crtvybu")
+    recipe = Recipe.objects.get_or_create(user=user)[0]
+    recipe.title = title
+    recipe.ingredients = ingredients
+    recipe.instructions = instructions
+    recipe.save()
+    return recipe
+
+class DatabaseRecipeTests(TestCase):
+
+    def test_recipe_upload_date(self):
+        test_recipe = add_recipe("Sushi", "this, that", "make it")
+        self.assertEqual(datetime.now().strftime("%x"), test_recipe.upload_date.strftime("%x"), "Upload date does not match current date.")
